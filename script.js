6bd4aac4a4ac0bcaa538ee4100c01364a6d1f3d5
rashida-mm <rashidamajeed00@gmail.com>
2023-11-14 20:01:25 +0530

    bmi-calc

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function resetForm() {
+  const bmiText = document.getElementById("bmi");
+  const resultText = document.getElementById("result");
+  bmiText.textContent = 0;
+  bmiText.className = "";
+  resultText.textContent = "";
+}
+
+function submitForm() {
+  const bmiText = document.getElementById("bmi");
+  const resultText = document.getElementById("result");
+  const form = document.getElementById("form");
+
+  event.preventDefault();
+  const weight = form.weight.value;
+  const height = form.height.value;
+
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+      alert("Please enter a valid weight and height");
+  }
+
+  const heightInMeters = height / 100; // cm -> m
+  const bmi = (weight / Math.pow(heightInMeters, 2)).toFixed(2);
+  bmiText.textContent = bmi;
+
+  const result = showBMI(bmi);
+  resultText.innerHTML = `You are <strong>${result}</strong>`;
+}
+
+function showBMI(bmi) {
+  if (bmi < 18.5) {
+      return "underweight";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+      return "healthy";
+  } else if (bmi > 25 && bmi < 29.9) {
+      return "overweight";
+  } else {
+      return "obese";
+  }
+}
