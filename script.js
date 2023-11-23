7fbc8c8eed100d1f6f52b94aa655630bf9829714
rashida-mm <rashidamajeed00@gmail.com>
2023-11-23 12:08:55 +0530

    bmi-calc

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ function submitForm() {
   const bmiText = document.getElementById("bmi");
   const resultText = document.getElementById("result");
 
-  
   const weight = parseFloat(document.getElementById("weight").value);
   const height = parseFloat(document.getElementById("height").value);
 
   if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
       alert("Please enter a valid weight and height");
   }
-
+else{
   const heightInMeters = height / 100; // cm -> m
   const bmi = (weight / Math.pow(heightInMeters, 2)).toFixed(2);
   bmiText.textContent = bmi;
 
   const result = showBMI(bmi);
   resultText.innerHTML = `You are <strong>${result}</strong>`;
+}
 }
 
 function showBMI(bmi) {
